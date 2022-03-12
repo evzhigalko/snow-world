945b07080edcfe1911042051ca6e7185b7e20383
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-03-12 11:11:57 +0400

    feat: added webmvc configs

---
+++ web/src/main/java/by/zhigalko/snow/world/config/WebApplicationInitializer.java
@@ -0,0 +1,21 @@
+package by.zhigalko.snow.world.config;
+
+import by.zhigalko.snow.world.util.ApplicationConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{ApplicationConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
