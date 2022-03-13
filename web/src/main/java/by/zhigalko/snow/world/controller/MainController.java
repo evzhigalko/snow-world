54acf5281f4d9242c79b9675f913252b687de6fd
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-03-13 16:44:00 +0400

    refactor: fixed main controller

--- web/src/main/java/by/zhigalko/snow/world/controller/MainController.java
+++ web/src/main/java/by/zhigalko/snow/world/controller/MainController.java
@@ -1,18 +1,42 @@
 package by.zhigalko.snow.world.controller;
 
-import by.zhigalko.snow.world.entity.enums.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
-    @GetMapping({"/snowboard", "/ski", "/clothes", "/contacts", "/form/login", "/form/registration", "/admin/"})
-    public String handleNavbar(HttpServletRequest request) {
-        Page page = Page.getEnum(request.getRequestURI());
-        return page.getForwardPage();
+    @GetMapping("/snowboard")
+    public String handleSnowboardGroup() {
+        return "snowboard-group";
+    }
+
+    @GetMapping("/ski")
+    public String handleSkiGroup() {
+        return "ski-group";
+    }
+
+    @GetMapping("/clothes")
+    public String handleClothesGroup() {
+        return "clothes-group";
+    }
+
+    @GetMapping("/contacts")
+    public String handleContacts() {
+        return "static/contacts";
+    }
+
+    @GetMapping("/form/login")
+    public String handleLogin() {
+        return "login";
+    }
+
+    @GetMapping("/form/registration")
+    public String handleRegistration() {
+        return "registration";
+    }
+
+    @GetMapping("/admin/")
+    public String handleAdministration() {
+        return "administration/admin";
     }
 }
