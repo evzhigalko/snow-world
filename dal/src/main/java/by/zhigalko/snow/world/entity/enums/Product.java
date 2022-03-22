0ff5c598b71e74f6085db93d5c449c6ce4ded3ea
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-03-22 20:18:52 +0400

    refactor: fixed enums after problem with commit

--- dal/src/main/java/by/zhigalko/snow/world/entity/enums/Product.java
+++ dal/src/main/java/by/zhigalko/snow/world/entity/enums/Product.java
@@ -1,6 +1,5 @@
 package by.zhigalko.snow.world.entity.enums;
 
-<<<<<<< HEAD
 public enum Product {
     SNOWBOARD("Сноуборд"),
     SNOWBOARD_BOOT("Ботинки сноубордные"),
@@ -24,36 +23,5 @@ public enum Product {
 
     public String getName() {
         return name;
-=======
-public enum ProductGroup {
-    EQUIPMENT,
-    BOOTS,
-    SKI,
-    SKI_POLE,
-    SNOWBOARD;
-
-    public static ProductGroup getProduct(String item) {
-        switch (item) {
-            case "jacket":
-            case "pants":
-            case "mask":
-            case "cap":
-            case "mitten":
-            case "glove":
-            case "ski_helmet":
-            case "snowboard_helmet":
-                return EQUIPMENT;
-            case "ski_boot":
-            case "snowboard_boot":
-                return BOOTS;
-            case "ski":
-                return SKI;
-            case "ski_pole":
-                return SKI_POLE;
-            case "snowboard":
-                return SNOWBOARD;
-        }
-        return null;
->>>>>>> b0b6416fe573512e4c6fa94e42262b09349dd95c
     }
 }
