0843df42edb4a6154c0162b33a73f052f3f7e413
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 14:38:04 +0400

    feat: added SnowboardHelmetRequest, SnowboardHelmetResponse

---
+++ service/src/main/java/by/zhigalko/snow/world/dto/item/request/SnowboardHelmetRequest.java
@@ -0,0 +1,11 @@
+package by.zhigalko.snow.world.dto.item.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class SnowboardHelmetRequest extends ItemRequest {}
---
+++ service/src/main/java/by/zhigalko/snow/world/dto/item/response/SnowboardHelmetResponse.java
@@ -0,0 +1,11 @@
+package by.zhigalko.snow.world.dto.item.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class SnowboardHelmetResponse extends ItemResponse{}
