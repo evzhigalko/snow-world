6e3419e509511aa52293af862ca3bedfa40d5774
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 14:11:33 +0400

    refactor: fixed AdminItemService, remove unnecessary methods

--- service/src/main/java/by/zhigalko/snow/world/service/common/AdminItemService.java
+++ service/src/main/java/by/zhigalko/snow/world/service/common/AdminItemService.java
@@ -1,16 +1,13 @@
 package by.zhigalko.snow.world.service.common;
 
-import by.zhigalko.snow.world.entity.EquipmentSize;
+import by.zhigalko.snow.world.dto.item.request.ItemRequest;
 import by.zhigalko.snow.world.entity.Image;
 import by.zhigalko.snow.world.entity.Item;
 import by.zhigalko.snow.world.entity.enums.*;
-import by.zhigalko.snow.world.service.common.equipment_size.EquipmentSizeService;
 import by.zhigalko.snow.world.service.common.image.ImageService;
-import by.zhigalko.snow.world.service.common.image.ImageServiceImpl;
 import by.zhigalko.snow.world.service.item.BaseItemService;
 import by.zhigalko.snow.world.service.item.ServiceEquipmentFactory;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,27 +21,24 @@ import java.util.UUID;
 public class AdminItemService {
     private final ServiceEquipmentFactory serviceEquipmentFactory;
     private final ImageService imageService;
-    private final EquipmentSizeService equipmentSizeService;
 
     @Autowired
-    public AdminItemService(ServiceEquipmentFactory serviceEquipmentFactory, ImageServiceImpl imageService, EquipmentSizeService equipmentSizeService) {
+    public AdminItemService(ServiceEquipmentFactory serviceEquipmentFactory, ImageService imageService) {
         this.serviceEquipmentFactory = serviceEquipmentFactory;
         this.imageService = imageService;
-        this.equipmentSizeService = equipmentSizeService;
     }
 
     @Transactional
-    public boolean addNewItem(HttpServletRequest request, MultipartFile filePart, String product) throws ServletException, IOException {
-        String equipmentSizeId = request.getParameter("equipment_size");
+    public boolean addNewItem(ItemRequest itemRequest, MultipartFile filePart, String product) throws ServletException, IOException {
         String fileName = filePart.getOriginalFilename();
         String imageName = imageService.uploadImage(filePart, product, fileName);
         Image image = imageService.getImage(imageName);
-        EquipmentSize equipmentSize = equipmentSizeService.findEquipmentSizeById(equipmentSizeId);
-        return save(request, product, image, equipmentSize);
-    }
-
-    private boolean save(HttpServletRequest request, String product, Image image, EquipmentSize equipmentSize) {
-        return saveItem(request, imageService, image, equipmentSize, serviceEquipmentFactory.getService(Product.valueOf(product.toUpperCase())));
+        BaseItemService service = serviceEquipmentFactory.getService(Product.valueOf(product.toUpperCase()));
+        Item item = service.getItem(itemRequest, image);
+        image.addItem(item);
+        imageService.save(image);
+        Item savedItem = service.save(item);
+        return savedItem!=null;
     }
 
     @Transactional
@@ -69,14 +63,6 @@ public class AdminItemService {
         if (!availableToRental.isEmpty()) {
             item.setAvailableToRental(Boolean.parseBoolean(availableToRental));
         }
-        Item savedItem = service.save(item);
-        return savedItem != null;
-    }
-
-    private boolean saveItem(HttpServletRequest request, ImageService imageService, Image image, EquipmentSize equipmentSize, BaseItemService service) {
-        Item item = service.getItem(request, equipmentSize, image);
-        image.addItem(item);
-        imageService.save(image);
         Item savedItem = service.save(item);
         return savedItem != null;
     }
