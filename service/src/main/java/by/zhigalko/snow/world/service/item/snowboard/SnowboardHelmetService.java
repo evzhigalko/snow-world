3719db718aa6f8ffb7b48afbfbe6a8afa1d203fc
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 15:08:04 +0400

    refactor: fixed functionality SnowboardHelmetService with mapper,  and linked jsp

--- service/src/main/java/by/zhigalko/snow/world/service/item/snowboard/SnowboardHelmetService.java
+++ service/src/main/java/by/zhigalko/snow/world/service/item/snowboard/SnowboardHelmetService.java
@@ -1,28 +1,30 @@
 package by.zhigalko.snow.world.service.item.snowboard;
 
-import by.zhigalko.snow.world.entity.EquipmentSize;
+import by.zhigalko.snow.world.dto.item.request.ItemRequest;
+import by.zhigalko.snow.world.dto.item.request.SnowboardHelmetRequest;
 import by.zhigalko.snow.world.entity.Image;
 import by.zhigalko.snow.world.entity.Item;
 import by.zhigalko.snow.world.entity.snowboard.SnowboardHelmet;
+import by.zhigalko.snow.world.mapper.SnowboardHelmetMapper;
 import by.zhigalko.snow.world.repository.EquipmentSizeRepository;
 import by.zhigalko.snow.world.repository.item.ItemRepository;
 import by.zhigalko.snow.world.service.item.BaseItemServiceImpl;
-import javax.servlet.http.HttpServletRequest;
-import by.zhigalko.snow.world.service.item.util.ItemGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SnowboardHelmetService extends BaseItemServiceImpl<SnowboardHelmet> {
-    private final ItemGenerator itemGenerator;
+    private final SnowboardHelmetMapper snowboardHelmetMapper;
 
     @Autowired
-    public SnowboardHelmetService(ItemRepository<SnowboardHelmet> itemRepository, EquipmentSizeRepository equipmentSizeRepository, ItemGenerator itemGenerator) {
+    public SnowboardHelmetService(ItemRepository<SnowboardHelmet> itemRepository, EquipmentSizeRepository equipmentSizeRepository, SnowboardHelmetMapper snowboardHelmetMapper) {
         super(itemRepository, equipmentSizeRepository);
-        this.itemGenerator = itemGenerator;
+        this.snowboardHelmetMapper = snowboardHelmetMapper;
     }
 
-    public Item getItem(HttpServletRequest request, EquipmentSize equipmentSize, Image image) {
-        return itemGenerator.setEquipmentData(request, new SnowboardHelmet(), equipmentSize, image);
+    public Item getItem(ItemRequest itemRequest, Image image) {
+        SnowboardHelmet snowboardHelmet = snowboardHelmetMapper.snowboardHelmetRequestToSnowboardHelmet((SnowboardHelmetRequest) itemRequest);
+        snowboardHelmet.setImage(image);
+        return snowboardHelmet;
     }
 }
