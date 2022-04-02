b5f29c864a2449ab5df359ae8f353b423ad41823
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 15:25:47 +0400

    feat: added SkiMapper and test

---
+++ service/src/main/java/by/zhigalko/snow/world/mapper/SkiMapper.java
@@ -0,0 +1,24 @@
+package by.zhigalko.snow.world.mapper;
+
+import by.zhigalko.snow.world.dto.item.request.SkiRequest;
+import by.zhigalko.snow.world.dto.item.response.SkiResponse;
+import by.zhigalko.snow.world.entity.enums.Product;
+import by.zhigalko.snow.world.entity.ski.Ski;
+import by.zhigalko.snow.world.service.common.equipment_size.EquipmentSizeService;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.UUID;
+
+@Mapper(componentModel = "spring", imports = {UUID.class, Product.class})
+public abstract class SkiMapper {
+    @Autowired
+    protected EquipmentSizeService equipmentSizeService;
+
+    @Mapping(expression = "java(Product.valueOf(skiRequest.getProductName()))", target = "productName")
+    @Mapping(expression = "java(equipmentSizeService.findEquipmentSizeById(skiRequest.getEquipmentSize()))",
+            target = "equipmentSize")
+    public abstract Ski skiRequestToSki(SkiRequest skiRequest);
+
+    public abstract SkiResponse skiToSkiResponse(Ski ski);
+}
---
+++ service/src/test/java/by/zhigalko/snow/world/mapper/SkiMapperTest.java
@@ -0,0 +1,83 @@
+package by.zhigalko.snow.world.mapper;
+
+import by.zhigalko.snow.world.dto.item.request.SkiRequest;
+import by.zhigalko.snow.world.dto.item.request.SnowboardRequest;
+import by.zhigalko.snow.world.dto.item.response.SkiResponse;
+import by.zhigalko.snow.world.dto.item.response.SnowboardResponse;
+import by.zhigalko.snow.world.entity.EquipmentSize;
+import by.zhigalko.snow.world.entity.Image;
+import by.zhigalko.snow.world.entity.enums.Gender;
+import by.zhigalko.snow.world.entity.enums.HardnessLevel;
+import by.zhigalko.snow.world.entity.enums.Product;
+import by.zhigalko.snow.world.entity.enums.RidingLevel;
+import by.zhigalko.snow.world.entity.ski.Ski;
+import by.zhigalko.snow.world.entity.snowboard.Snowboard;
+import by.zhigalko.snow.world.service.common.equipment_size.EquipmentSizeService;
+import by.zhigalko.snow.world.util.ApplicationConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = {ApplicationConfig.class, BCryptPasswordEncoder.class})
+@Transactional
+class SkiMapperTest {
+
+    @Autowired
+    private SkiMapper skiMapper;
+
+    @Autowired
+    private EquipmentSizeService equipmentSizeService;
+
+
+    @Test
+    void skiRequestToSkiTest() {
+        SkiRequest skiRequest = new SkiRequest();
+        skiRequest.setCost("15.0");
+        skiRequest.setGender("FEMALE");
+        skiRequest.setMaker("PRIME");
+        skiRequest.setRidingLevel("BEGINNER");
+        skiRequest.setProductName("SKI");
+        skiRequest.setAvailableToRental("true");
+        skiRequest.setEquipmentSize("SK183");
+
+        Ski ski = skiMapper.skiRequestToSki(skiRequest);
+
+        assertNotNull(ski);
+        assertEquals(skiRequest.getEquipmentSize(), ski.getEquipmentSize().getEquipmentSizeId());
+        assertEquals(skiRequest.getRidingLevel(), ski.getRidingLevel().toString());
+        System.out.println(skiRequest);
+        System.out.println(ski);
+    }
+
+    @Test
+    void skiToSkiResponseTest() {
+        Ski ski = new Ski();
+        ski.setId(UUID.randomUUID());
+        ski.setCost(15.0);
+        ski.setGender(Gender.FEMALE);
+        ski.setMaker("PRIME");
+        ski.setRidingLevel(RidingLevel.BEGINNER);
+        ski.setProductName(Product.SKI);
+        ski.setAvailableToRental(true);
+        Image image = new Image();
+        image.setId(UUID.randomUUID());
+        image.setImageName("IMAGE_NAME_TEST.jpg");
+        ski.setImage(image);
+        EquipmentSize equipmentSize = equipmentSizeService.findEquipmentSizeById("SK183");
+        ski.setEquipmentSize(equipmentSize);
+
+        SkiResponse skiResponse = skiMapper.skiToSkiResponse(ski);
+
+        assertNotNull(skiResponse);
+        assertEquals(ski.getEquipmentSize().getEquipmentSizeId(), skiResponse.getEquipmentSize().getEquipmentSizeId());
+        assertEquals(ski.getId(), skiResponse.getId());
+        System.out.println(ski);
+        System.out.println(skiResponse);
+    }
+}
