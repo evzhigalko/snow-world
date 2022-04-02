acfbe4168f85e48d4906d7cbffcaf57e90e51511
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 16:35:21 +0400

    refactor: fixed functionality SkiBootService with mapper and linked jsp

--- service/src/main/java/by/zhigalko/snow/world/service/item/ski/SkiBootService.java
+++ service/src/main/java/by/zhigalko/snow/world/service/item/ski/SkiBootService.java
@@ -1,29 +1,31 @@
 package by.zhigalko.snow.world.service.item.ski;
 
-import by.zhigalko.snow.world.entity.EquipmentSize;
+import by.zhigalko.snow.world.dto.item.request.ItemRequest;
+import by.zhigalko.snow.world.dto.item.request.SkiBootRequest;
 import by.zhigalko.snow.world.entity.Image;
 import by.zhigalko.snow.world.entity.Item;
 import by.zhigalko.snow.world.entity.ski.SkiBoot;
+import by.zhigalko.snow.world.mapper.SkiBootMapper;
 import by.zhigalko.snow.world.repository.EquipmentSizeRepository;
 import by.zhigalko.snow.world.repository.item.ItemRepository;
 import by.zhigalko.snow.world.service.item.BaseItemServiceImpl;
-import javax.servlet.http.HttpServletRequest;
-import by.zhigalko.snow.world.service.item.util.ItemGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SkiBootService extends BaseItemServiceImpl<SkiBoot> {
-    private final ItemGenerator itemGenerator;
+    private final SkiBootMapper skiBootMapper;
 
     @Autowired
-    public SkiBootService(ItemRepository<SkiBoot> itemRepository, EquipmentSizeRepository equipmentSizeRepository, ItemGenerator itemGenerator) {
+    public SkiBootService(ItemRepository<SkiBoot> itemRepository, EquipmentSizeRepository equipmentSizeRepository, SkiBootMapper skiBootMapper) {
         super(itemRepository, equipmentSizeRepository);
-        this.itemGenerator = itemGenerator;
+        this.skiBootMapper = skiBootMapper;
     }
 
     @Override
-    public Item getItem(HttpServletRequest request, EquipmentSize equipmentSize, Image image) {
-        return itemGenerator.setEquipmentData(request, new SkiBoot(), equipmentSize, image);
+    public Item getItem(ItemRequest itemRequest, Image image) {
+        SkiBoot skiBoot = skiBootMapper.skiBootRequestToSkiBoot((SkiBootRequest) itemRequest);
+        skiBoot.setImage(image);
+        return skiBoot;
     }
 }
--- web/src/main/webapp/WEB-INF/jsp/administration/new-ski-boot.jsp
+++ web/src/main/webapp/WEB-INF/jsp/administration/new-ski-boot.jsp
@@ -2,7 +2,7 @@
 <div style="padding-top: 3rem">
     <p>Ботинки лыжные</p>
     <form action="<c:url value="/admin/create/new/ski-boot"/>" method="post" enctype="multipart/form-data" style="width: 60%">
-        <input type="hidden" name="product_group" value="SKI_BOOT">
+        <input type="hidden" name="productName" value="SKI_BOOT">
         <div class="form-input">
             <label for="label-maker" class="form-label"> </label>
             <input id="label-maker" style="width: 15rem" type="text" class="form-control" placeholder="Производитель"
@@ -24,7 +24,7 @@
         <div class="form-input">
             Доступность к прокату:
             <label>
-                <select class="form-select" name="available_to_rental">
+                <select class="form-select" name="availableToRental">
                     <option value="true">Да</option>
                     <option value="false">Нет</option>
                 </select>
@@ -33,7 +33,7 @@
         <div class="form-input">
             Размер:
             <label>
-                <select class="form-select" name="equipment_size">
+                <select class="form-select" name="equipmentSize">
                     <c:forEach items="${requestScope.allSizes}" var="size">
                         <option value="${size.equipmentSizeId}">${size.equipmentSizeId}</option>
                     </c:forEach>
