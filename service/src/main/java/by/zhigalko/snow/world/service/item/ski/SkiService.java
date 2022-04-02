05457cc7105570b996d16c243e8dd7b7105b42d4
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 15:34:00 +0400

    refactor: fixed functionality SkiService with mapper, and linked jsp

--- service/src/main/java/by/zhigalko/snow/world/service/item/ski/SkiService.java
+++ service/src/main/java/by/zhigalko/snow/world/service/item/ski/SkiService.java
@@ -1,32 +1,31 @@
 package by.zhigalko.snow.world.service.item.ski;
 
-import by.zhigalko.snow.world.entity.EquipmentSize;
+import by.zhigalko.snow.world.dto.item.request.ItemRequest;
+import by.zhigalko.snow.world.dto.item.request.SkiRequest;
 import by.zhigalko.snow.world.entity.Image;
 import by.zhigalko.snow.world.entity.Item;
-import by.zhigalko.snow.world.entity.enums.RidingLevel;
 import by.zhigalko.snow.world.entity.ski.Ski;
+import by.zhigalko.snow.world.mapper.SkiMapper;
 import by.zhigalko.snow.world.repository.EquipmentSizeRepository;
 import by.zhigalko.snow.world.repository.item.ItemRepository;
 import by.zhigalko.snow.world.service.item.BaseItemServiceImpl;
-import javax.servlet.http.HttpServletRequest;
-import by.zhigalko.snow.world.service.item.util.ItemGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SkiService extends BaseItemServiceImpl<Ski> {
-    private final ItemGenerator itemGenerator;
+    private final SkiMapper skiMapper;
 
     @Autowired
-    public SkiService(ItemRepository<Ski> itemRepository, EquipmentSizeRepository equipmentSizeRepository, ItemGenerator itemGenerator) {
+    public SkiService(ItemRepository<Ski> itemRepository, EquipmentSizeRepository equipmentSizeRepository, SkiMapper skiMapper) {
         super(itemRepository, equipmentSizeRepository);
-        this.itemGenerator = itemGenerator;
+        this.skiMapper = skiMapper;
     }
 
     @Override
-    public Item getItem(HttpServletRequest request, EquipmentSize equipmentSize, Image image) {
-        Ski ski = new Ski();
-        ski.setRidingLevel(RidingLevel.valueOf(request.getParameter("riding_level")));
-        return itemGenerator.setEquipmentData(request, ski, equipmentSize, image);
+    public Item getItem(ItemRequest itemRequest, Image image) {
+        Ski ski = skiMapper.skiRequestToSki((SkiRequest) itemRequest);
+        ski.setImage(image);
+        return ski;
     }
 }
--- web/src/main/webapp/WEB-INF/jsp/administration/new-ski.jsp
+++ web/src/main/webapp/WEB-INF/jsp/administration/new-ski.jsp
@@ -2,6 +2,7 @@
 <div style="padding-top: 3rem">
     <p>Лыжи</p>
     <form action="<c:url value="/admin/create/new/ski"/>" method="post" enctype="multipart/form-data" style="width: 60%">
+        <input type="hidden" name="productName" value="SKI">
         <div class="form-input">
             <label for="label-maker" class="form-label"> </label>
             <input id="label-maker" style="width: 15rem" type="text" class="form-control" placeholder="Производитель"
@@ -23,7 +24,7 @@
         <div class="form-input">
             Доступность к прокату:
             <label>
-                <select class="form-select" name="available_to_rental">
+                <select class="form-select" name="availableToRental">
                     <option value="true">Да</option>
                     <option value="false">Нет</option>
                 </select>
@@ -32,7 +33,7 @@
         <div class="form-input">
             Размер:
             <label>
-                <select class="form-select" name="equipment_size">
+                <select class="form-select" name="equipmentSize">
                     <c:forEach items="${requestScope.allSizes}" var="size">
                         <option value="${size.equipmentSizeId}">${size.equipmentSizeId}
                             (Рост: ${size.userMinHeight}-${size.userMaxHeight})
@@ -41,11 +42,10 @@
                 </select>
             </label>
         </div>
-        <input type="hidden" name="product_group" value="SKI">
         <div class="form-input">
             Уровень катания:
             <label>
-                <select class="form-select" name="riding_level">
+                <select class="form-select" name="ridingLevel">
                     <option value="BEGINNER">Новичок</option>
                     <option value="MEDIUM">Средний</option>
                     <option value="HIGH">Высокий</option>
