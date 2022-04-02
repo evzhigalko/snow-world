9e3dc30ecbc223225959dd7ad968dd7907ad000e
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 18:29:30 +0400

    refactor: fixed functionality CapService with mapper

--- service/src/main/java/by/zhigalko/snow/world/service/item/clothes/CapService.java
+++ service/src/main/java/by/zhigalko/snow/world/service/item/clothes/CapService.java
@@ -1,27 +1,29 @@
 package by.zhigalko.snow.world.service.item.clothes;
 
-import by.zhigalko.snow.world.entity.EquipmentSize;
+import by.zhigalko.snow.world.dto.item.request.CapRequest;
+import by.zhigalko.snow.world.dto.item.request.ItemRequest;
 import by.zhigalko.snow.world.entity.Image;
 import by.zhigalko.snow.world.entity.Item;
 import by.zhigalko.snow.world.entity.clothes.Cap;
+import by.zhigalko.snow.world.mapper.CapMapper;
 import by.zhigalko.snow.world.repository.EquipmentSizeRepository;
 import by.zhigalko.snow.world.repository.item.ItemRepository;
 import by.zhigalko.snow.world.service.item.BaseItemServiceImpl;
-import javax.servlet.http.HttpServletRequest;
-import by.zhigalko.snow.world.service.item.util.ItemGenerator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CapService extends BaseItemServiceImpl<Cap> {
-    private final ItemGenerator itemGenerator;
+    private final CapMapper capMapper;
 
-    public CapService(ItemRepository<Cap> itemRepository, EquipmentSizeRepository equipmentSizeRepository, ItemGenerator itemGenerator) {
+    public CapService(ItemRepository<Cap> itemRepository, EquipmentSizeRepository equipmentSizeRepository, CapMapper capMapper) {
         super(itemRepository, equipmentSizeRepository);
-        this.itemGenerator = itemGenerator;
+        this.capMapper = capMapper;
     }
 
     @Override
-    public Item getItem(HttpServletRequest request, EquipmentSize equipmentSize, Image image) {
-        return itemGenerator.setEquipmentData(request, new Cap(), equipmentSize, image);
+    public Item getItem(ItemRequest itemRequest, Image image) {
+        Cap cap = capMapper.capRequestToCap((CapRequest) itemRequest);
+        cap.setImage(image);
+        return cap;
     }
 }
