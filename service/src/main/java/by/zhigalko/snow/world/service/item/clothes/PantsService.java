dd016c616b93a56c69740b5ad00eb3f102124cf6
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 18:08:25 +0400

    refactor: fixed functionality PantsService with mapper

--- service/src/main/java/by/zhigalko/snow/world/service/item/clothes/PantsService.java
+++ service/src/main/java/by/zhigalko/snow/world/service/item/clothes/PantsService.java
@@ -1,29 +1,31 @@
 package by.zhigalko.snow.world.service.item.clothes;
 
-import by.zhigalko.snow.world.entity.EquipmentSize;
+import by.zhigalko.snow.world.dto.item.request.ItemRequest;
+import by.zhigalko.snow.world.dto.item.request.PantsRequest;
 import by.zhigalko.snow.world.entity.Image;
 import by.zhigalko.snow.world.entity.Item;
 import by.zhigalko.snow.world.entity.clothes.Pants;
+import by.zhigalko.snow.world.mapper.PantsMapper;
 import by.zhigalko.snow.world.repository.EquipmentSizeRepository;
 import by.zhigalko.snow.world.repository.item.ItemRepository;
 import by.zhigalko.snow.world.service.item.BaseItemServiceImpl;
-import javax.servlet.http.HttpServletRequest;
-import by.zhigalko.snow.world.service.item.util.ItemGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PantsService extends BaseItemServiceImpl<Pants> {
-    private final ItemGenerator itemGenerator;
+    private final PantsMapper pantsMapper;
 
     @Autowired
-    public PantsService(ItemRepository<Pants> itemRepository, EquipmentSizeRepository equipmentSizeRepository, ItemGenerator itemGenerator) {
+    public PantsService(ItemRepository<Pants> itemRepository, EquipmentSizeRepository equipmentSizeRepository, PantsMapper pantsMapper) {
         super(itemRepository, equipmentSizeRepository);
-        this.itemGenerator = itemGenerator;
+        this.pantsMapper = pantsMapper;
     }
 
     @Override
-    public Item getItem(HttpServletRequest request, EquipmentSize equipmentSize, Image image) {
-        return itemGenerator.setEquipmentData(request, new Pants(), equipmentSize, image);
+    public Item getItem(ItemRequest itemRequest, Image image) {
+        Pants pants = pantsMapper.pantsRequestToPants((PantsRequest) itemRequest);
+        pants.setImage(image);
+        return pants;
     }
 }
