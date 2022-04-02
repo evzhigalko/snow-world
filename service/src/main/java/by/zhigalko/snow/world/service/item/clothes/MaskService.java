b498ca201c44b09c00d967edc7919d4eb0ff9c44
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 18:43:57 +0400

    refactor: fixed functionality MaskService with mapper

--- service/src/main/java/by/zhigalko/snow/world/service/item/clothes/MaskService.java
+++ service/src/main/java/by/zhigalko/snow/world/service/item/clothes/MaskService.java
@@ -1,29 +1,31 @@
 package by.zhigalko.snow.world.service.item.clothes;
 
-import by.zhigalko.snow.world.entity.EquipmentSize;
+import by.zhigalko.snow.world.dto.item.request.ItemRequest;
+import by.zhigalko.snow.world.dto.item.request.MaskRequest;
 import by.zhigalko.snow.world.entity.Image;
 import by.zhigalko.snow.world.entity.Item;
 import by.zhigalko.snow.world.entity.clothes.Mask;
+import by.zhigalko.snow.world.mapper.MaskMapper;
 import by.zhigalko.snow.world.repository.EquipmentSizeRepository;
 import by.zhigalko.snow.world.repository.item.ItemRepository;
 import by.zhigalko.snow.world.service.item.BaseItemServiceImpl;
-import javax.servlet.http.HttpServletRequest;
-import by.zhigalko.snow.world.service.item.util.ItemGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MaskService extends BaseItemServiceImpl<Mask> {
-    private final ItemGenerator itemGenerator;
+    private final MaskMapper maskMapper;
 
     @Autowired
-    public MaskService(ItemRepository<Mask> itemRepository, EquipmentSizeRepository equipmentSizeRepository, ItemGenerator itemGenerator) {
+    public MaskService(ItemRepository<Mask> itemRepository, EquipmentSizeRepository equipmentSizeRepository, MaskMapper maskMapper) {
         super(itemRepository, equipmentSizeRepository);
-        this.itemGenerator = itemGenerator;
+        this.maskMapper = maskMapper;
     }
 
     @Override
-    public Item getItem(HttpServletRequest request, EquipmentSize equipmentSize, Image image) {
-        return itemGenerator.setEquipmentData(request, new Mask(), equipmentSize, image);
+    public Item getItem(ItemRequest itemRequest, Image image) {
+        Mask mask = maskMapper.maskRequestToMask((MaskRequest) itemRequest);
+        mask.setImage(image);
+        return mask;
     }
 }
