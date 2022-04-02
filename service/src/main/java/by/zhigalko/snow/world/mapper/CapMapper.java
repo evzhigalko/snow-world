88cf981e3b3c31ef11c92f5e9baf2bb0d8b15ba0
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-02 18:25:11 +0400

    feat: added CapRequest/Response, CapMapper and test

---
+++ service/src/main/java/by/zhigalko/snow/world/mapper/CapMapper.java
@@ -0,0 +1,24 @@
+package by.zhigalko.snow.world.mapper;
+
+import by.zhigalko.snow.world.dto.item.request.CapRequest;
+import by.zhigalko.snow.world.dto.item.response.CapResponse;
+import by.zhigalko.snow.world.entity.clothes.Cap;
+import by.zhigalko.snow.world.entity.enums.Product;
+import by.zhigalko.snow.world.service.common.equipment_size.EquipmentSizeService;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.UUID;
+
+@Mapper(componentModel = "spring", imports = {UUID.class, Product.class})
+public abstract class CapMapper {
+    @Autowired
+    protected EquipmentSizeService equipmentSizeService;
+
+    @Mapping(expression = "java(Product.valueOf(capRequest.getProductName()))", target = "productName")
+    @Mapping(expression = "java(equipmentSizeService.findEquipmentSizeById(capRequest.getEquipmentSize()))",
+            target = "equipmentSize")
+    public abstract Cap capRequestToCap(CapRequest capRequest);
+
+    public abstract CapResponse capToCapResponse(Cap cap);
+}
---
+++ service/src/test/java/by/zhigalko/snow/world/mapper/CapMapperTest.java
@@ -0,0 +1,74 @@
+package by.zhigalko.snow.world.mapper;
+
+import by.zhigalko.snow.world.dto.item.request.CapRequest;
+import by.zhigalko.snow.world.dto.item.response.CapResponse;
+import by.zhigalko.snow.world.entity.EquipmentSize;
+import by.zhigalko.snow.world.entity.Image;
+import by.zhigalko.snow.world.entity.clothes.Cap;
+import by.zhigalko.snow.world.entity.enums.Gender;
+import by.zhigalko.snow.world.entity.enums.Product;
+import by.zhigalko.snow.world.service.common.equipment_size.EquipmentSizeService;
+import by.zhigalko.snow.world.util.ApplicationConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = {ApplicationConfig.class, BCryptPasswordEncoder.class})
+@Transactional
+class CapMapperTest {
+    @Autowired
+    private CapMapper capMapper;
+
+    @Autowired
+    private EquipmentSizeService equipmentSizeService;
+
+    @Test
+    void capRequestToCapTest() {
+        CapRequest capRequest = new CapRequest();
+        capRequest.setCost("15");
+        capRequest.setAvailableToRental("true");
+        capRequest.setGender("FEMALE");
+        capRequest.setEquipmentSize("XL");
+        capRequest.setMaker("QUIKSILVER");
+        capRequest.setProductName("CAP");
+
+        Cap cap = capMapper.capRequestToCap(capRequest);
+
+        assertNotNull(cap);
+        assertEquals(capRequest.getEquipmentSize(), cap.getEquipmentSize().getEquipmentSizeId());
+        assertEquals(capRequest.getMaker(), cap.getMaker());
+        assertEquals(capRequest.getProductName(), cap.getProductName().toString());
+        System.out.println(capRequest);
+        System.out.println(cap);
+    }
+
+    @Test
+    void capToCapResponseTest() {
+        Cap cap = new Cap();
+        cap.setCost(15);
+        cap.setId(UUID.randomUUID());
+        cap.setAvailableToRental(true);
+        cap.setGender(Gender.FEMALE);
+        cap.setMaker("QUIKSILVER");
+        cap.setProductName(Product.CAP);
+        Image image = new Image();
+        image.setId(UUID.randomUUID());
+        image.setImageName("IMAGE_NAME_TEST.jpg");
+        cap.setImage(image);
+        EquipmentSize equipmentSize = equipmentSizeService.findEquipmentSizeById("XL");
+        cap.setEquipmentSize(equipmentSize);
+
+        CapResponse capResponse = capMapper.capToCapResponse(cap);
+
+        assertNotNull(cap);
+        assertEquals(cap.getEquipmentSize().getEquipmentSizeId(), capResponse.getEquipmentSize().getEquipmentSizeId());
+        assertEquals(cap.getMaker(), capResponse.getMaker());
+        assertEquals(cap.getProductName().toString(), capResponse.getProductName());
+        System.out.println(cap);
+        System.out.println(capResponse);
+    }
+}
