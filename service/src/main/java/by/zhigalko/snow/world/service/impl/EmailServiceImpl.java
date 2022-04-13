17b473043e75b79124e88168961816c9c9ac3688
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-04-13 17:06:34 +0400

    feat: added functionality to EmailService to send message to email from order

--- service/src/main/java/by/zhigalko/snow/world/service/impl/EmailServiceImpl.java
+++ service/src/main/java/by/zhigalko/snow/world/service/impl/EmailServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -30,24 +31,46 @@ public class EmailServiceImpl implements EmailService {
     public void sendMessage(OrderDetailsDto orderDetailsDto) {
         SimpleMailMessage message = createEmail(orderDetailsDto);
         emailSender.send(message);
+        SimpleMailMessage messageToUser = createEmailToUser(orderDetailsDto);
+        emailSender.send(messageToUser);
     }
 
     private SimpleMailMessage createEmail(OrderDetailsDto orderDetailsDto) {
         OrderResponse order = orderService.findById(UUID.fromString(orderDetailsDto.getOrderId()));
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setSubject("НОВЫЙ ЗАКАЗ: №" + orderDetailsDto.getOrderId() + ", дата: " + LocalDateTime.now());
         message.setTo(Objects.requireNonNull(template.getTo())[0]);
+        message.setSubject("НОВЫЙ ЗАКАЗ: №" + orderDetailsDto.getOrderId() + ", дата: " + LocalDateTime.now());
         StringBuilder contacts = new StringBuilder();
         contacts
                 .append("Контактная информация заказчика: \n")
                 .append("Фамилия: ").append(orderDetailsDto.getLastname()).append(".\n")
                 .append("Имя: ").append(orderDetailsDto.getFirstname()).append(".\n")
                 .append("Номер телефона: ").append(orderDetailsDto.getPhoneNumber()).append(".\n")
-                .append( "Электронный адрес: ").append(orderDetailsDto.getEmail()).append(".\n");
-        String format = String.format("Поступил новый заказ № %s.\nДата начала бронирования: %s.\n" +
+                .append("Электронный адрес: ").append(orderDetailsDto.getEmail()).append(".\n");
+        String newOrderDetails = String.format("\nПоступил новый заказ № %s.\nДата начала бронирования: %s.\n" +
                         "Дней бронирования: %s.\nОбщая сумма заказа: %s\n",
-                order.getOrderId(), order.getStartReservationDate(), order.getReservationDayNumber(), order.getTotalSum());
+                order.getOrderId(), order.getStartReservationDate().format(getDateTimeFormatter()), order.getReservationDayNumber(), order.getTotalSum());
         String itemDetailsHeader = "\nЗаказанная экипировка: \n";
+        String itemDetails = getItemDetails(order);
+        message.setText(contacts + newOrderDetails + itemDetailsHeader + itemDetails);
+        return message;
+    }
+
+    private SimpleMailMessage createEmailToUser(OrderDetailsDto orderDetailsDto) {
+        OrderResponse order = orderService.findById(UUID.fromString(orderDetailsDto.getOrderId()));
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(orderDetailsDto.getEmail());
+        message.setSubject("Snow-World: Ваш заказ № " + order.getOrderId());
+        String orderDetails = "\nВы успешно оформили заказ в Snow-World.\nНомер вашего заказа:\n" + order.getOrderId() + ".\n" +
+                "Предъявите этот номер администратору в салоне-проката.\nДата начала бронирования: " +
+                order.getStartReservationDate().format(getDateTimeFormatter()) + "\n";
+        String itemDetailsHeader = "\nВаш заказ:\n";
+        String itemDetails = getItemDetails(order);
+        message.setText(orderDetails + itemDetailsHeader + itemDetails);
+        return message;
+    }
+
+    private String getItemDetails(OrderResponse order) {
         StringBuilder itemDetails = new StringBuilder();
         for (Item item : order.getItems()) {
             itemDetails
@@ -58,7 +81,11 @@ public class EmailServiceImpl implements EmailService {
                     .append("Производитель: ").append(item.getMaker()).append(".\n")
                     .append("Размер: ").append(item.getEquipmentSize().getEquipmentSizeId()).append(".\n");
         }
-        message.setText(contacts + format + itemDetailsHeader + itemDetails);
-        return message;
+        return itemDetails.toString();
+    }
+
+    private DateTimeFormatter getDateTimeFormatter() {
+        String datePattern = "dd.MM.yyyy";
+        return DateTimeFormatter.ofPattern(datePattern);
     }
 }
