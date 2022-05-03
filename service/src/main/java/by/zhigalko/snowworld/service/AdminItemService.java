8438013a5fee502535669b99cb79f6caa756f897
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-05-03 21:12:01 +0400

    refactor: AdminItemService and impl

--- service/src/main/java/by/zhigalko/snowworld/service/AdminItemService.java
+++ service/src/main/java/by/zhigalko/snowworld/service/AdminItemService.java
@@ -1,9 +1,7 @@
 package by.zhigalko.snowworld.service;
 
 import by.zhigalko.snowworld.dto.request.ItemRequest;
-import by.zhigalko.snowworld.service.util.ServiceEquipmentFactory;
 import org.springframework.web.multipart.MultipartFile;
-import javax.servlet.ServletException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -15,19 +13,18 @@ public interface AdminItemService {
      * Add new item from admin credentials
      * @param itemRequest {@link ItemRequest} is received from presentation layer
      * @param filePart    {@link MultipartFile} uploaded image, which will be saved  into database
-     * @param product     name of product, which defined to call necessary service from ${@link ServiceEquipmentFactory}
+     * @param product     name of product
      * @return <ul>
      * <li>{@code true} if saved</li>
      * <li>{@code false} if didn't save</li>
      * </ul>
-     * @throws ServletException
      * @throws IOException
      */
-    boolean addNewItem(ItemRequest itemRequest, MultipartFile filePart, String product) throws ServletException, IOException;
+    boolean addNewItem(ItemRequest itemRequest, MultipartFile filePart, String product) throws IOException;
 
     /**
      * Delete item from admin credentials
-     * @param product     name of product, which defined to call necessary service from ${@link ServiceEquipmentFactory}
+     * @param product     name of product
      * @param id {@link UUID} is used to define item to delete
      */
     void deleteItem(String product, UUID id);
@@ -37,8 +34,10 @@ public interface AdminItemService {
      * @param cost of item,
      * @param availableToRental of item,
      * @param id {@link UUID} is used to define item to update
-     * @param service {@link BaseItemService}
-     * @return
+     * @return <ul>
+     * <li>{@code true} if saved</li>
+     * <li>{@code false} if didn't save</li>
+     * </ul>
      */
-    boolean updateItem(String cost, String availableToRental, UUID id, BaseItemService service);
+    boolean updateItem(String cost, String availableToRental, UUID id);
 }
