dbe08406e47ba4a5bdc9c260a86780b76e08ed87
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-05-07 14:09:59 +0400

    feat: added filter for mapping anyhttp methods

--- web/src/main/java/by/zhigalko/snowworld/config/WebApplicationInitializer.java
+++ web/src/main/java/by/zhigalko/snowworld/config/WebApplicationInitializer.java
@@ -1,6 +1,9 @@
 package by.zhigalko.snowworld.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,5 +19,16 @@ public class WebApplicationInitializer extends AbstractAnnotationConfigDispatche
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
