a2f6d14f81ef177874957731f5a5c9008b7b8861
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-05-16 12:45:04 +0400

    refactor: integration test in RoleRepository to unit test

--- dal/src/test/java/by/zhigalko/snowworld/repository/RoleRepositoryTest.java
+++ dal/src/test/java/by/zhigalko/snowworld/repository/RoleRepositoryTest.java
@@ -2,49 +2,35 @@ package by.zhigalko.snowworld.repository;
 
 import by.zhigalko.snowworld.entity.Role;
 import by.zhigalko.snowworld.model.RoleName;
-import by.zhigalko.snowworld.config.ApplicationConfig;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.transaction.annotation.Transactional;
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.UUID;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-@SpringJUnitConfig(classes = ApplicationConfig.class)
-@Rollback
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class RoleRepositoryTest {
-    private Role expected;
+    private Role role;
 
-    @Autowired
+    @Mock
     private RoleRepository roleRepository;
-
-    @Autowired
-    private LocalContainerEntityManagerFactoryBean entityManagerFactoryBean;
-
-    private EntityManager entityManager;
 
     @BeforeEach
     void setUp(){
-        entityManager = entityManagerFactoryBean.getNativeEntityManagerFactory().createEntityManager();
-        TypedQuery<Role> query = entityManager.createQuery("select r from Role as r where roleName = :roleName", Role.class);
-        query.setParameter("roleName", RoleName.USER);
-        expected = query.getSingleResult();
+        role = new Role();
+        role.setRoleName(RoleName.USER);
+        role.setId(UUID.randomUUID());
     }
 
     @Test
     void findByRoleNameTest() {
-        Role actual = roleRepository.findByRoleName(RoleName.USER);
+        doReturn(role).when(roleRepository).findByRoleName(RoleName.USER);
 
-        assertNotNull(actual);
-        Assertions.assertEquals(expected.getRoleName(), actual.getRoleName());
-        assertEquals(expected, actual);
-        System.out.println(expected);
-        System.out.println(actual);
+        Role actualRole = roleRepository.findByRoleName(RoleName.USER);
+
+        assertThat(actualRole).isEqualTo(role);
     }
 }
